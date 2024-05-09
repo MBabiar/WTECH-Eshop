c6159f540807283e07e1f00e6487dabaead3fc68
Mário Babiar <mariobabiar11@gmail.com>
2024-05-09 09:48:31 +0200

    +

--- AnimeHaven/routes/web.php
+++ AnimeHaven/routes/web.php
@@ -17,12 +17,12 @@ Route::middleware('guest')->group(function () {
     // Register
     Route::get('/register', [RegisteredUserController::class, 'create'])
         ->name('register');
-    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::post('/register', [RegisteredUserController::class, 'store'])->name('register');
 
     //Login
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
-    Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('/login', [AuthenticatedSessionController::class, 'store'])->name('login');
 });
 
 // Authenticated
