75faf12ddf0e83bc49868b240828538585369565
Peter Brandajský <xbrandajsky@stuba.sk>
2024-05-10 20:32:07 +0200

    +uprava zabudnutia hesla

--- AnimeHaven/resources/views/auth/forgot-password.blade.php
+++ AnimeHaven/resources/views/auth/forgot-password.blade.php
@@ -1,32 +1,31 @@
 <x-app-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    <div class="form-container">
+        @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <!-- Email Address -->
+            <div class="mb-3">
+                <div class="row">
+                    <label for="email" class="col-3 col-form-label">{{ __('Email') }}</label>
+                    <div class="col-8">
+                        <input id="email" class="form-control" type="email" name="email" :value="old('email')"
+                            required autofocus />
+                    </div>
+                </div>
+            </div>
+
+            <div class="flex items-center justify-end mt-4">
+                <button class="btn btn-primary">
+                    {{ __('Email Password Reset Link') }}
+                </button>
+            </div>
+        </form>
     </div>
 
-    <!-- Session Status -->
-    @if (session('status'))
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ session('status') }}
-        </div>
-    @endif
-
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <label for="email" class="block font-medium text-sm text-gray-700">{{ __('Email') }}</label>
-            <input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                autofocus />
-            @error('email')
-                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-            @enderror
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <button class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500">
-                {{ __('Email Password Reset Link') }}
-            </button>
-        </div>
-    </form>
 </x-app-layout>
--- AnimeHaven/resources/views/auth/reset-password.blade.php
+++ AnimeHaven/resources/views/auth/reset-password.blade.php
@@ -1,33 +1,39 @@
 <x-app-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+    <div class="form-container">
+        <form method="POST" action="{{ route('password.store') }}">
+            @csrf
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
-        <input type="hidden" name="email" value="{{ $request->email }}">
+            <!-- Password Reset Token -->
+            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+            <input type="hidden" name="email" value="{{ $request->email }}">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <label for="password" class="block font-medium text-sm text-gray-700">{{ __('Password') }}</label>
-            <input id="password" class="block mt-1 w-full" type="password" name="password" required />
-            @error('password')
-                <p class="mt-2 text-sm text-red-600">{{ $message }}</p>
-            @enderror
-        </div>
+            <!-- Password -->
+            <div class="mb-3">
+                <div class="row">
+                    <label for="password" class="col-3 col-form-label">{{ __('Password') }}</label>
+                    <div class="col-8">
+                        <input id="password" class="form-control" type="password" name="password" required />
+                    </div>
+                </div>
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <label for="password_confirmation"
-                class="block font-medium text-sm text-gray-700">{{ __('Confirm Password') }}</label>
-            <input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation"
-                required />
-        </div>
+            <!-- Confirm Password -->
+            <div class="mb-3">
+                <div class="row">
+                    <label for="password_confirmation" class="col-3 col-form-label">{{ __('Confirm Password') }}</label>
+                    <div class="col-8">
+                        <input id="password_confirmation" class="form-control" type="password"
+                            name="password_confirmation" required />
+                    </div>
+                </div>
+            </div>
 
-        <div class="flex items
-        -center justify-end mt-4">
-            <button class="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-500">
-                {{ __('Reset Password') }}
-            </button>
-        </div>
-    </form>
+            <div class="flex items
+            -center justify-end mt-4">
+                <button class="btn btn-primary">
+                    {{ __('Reset Password') }}
+                </button>
+            </div>
+        </form>
+    </div>
 </x-app-layout>
